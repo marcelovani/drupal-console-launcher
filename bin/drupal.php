8cdb59c82915bf26a2f33d9f26628abb55b724b4
davidgrayston <david_grayston@dennis.co.uk>
2017-01-11 12:08:17 +0000

    Reverting code and version changes

--- bin/drupal.php
+++ bin/drupal.php
@@ -13,27 +13,18 @@ use Drupal\Console\Launcher\Utils\Remote;
 
 set_time_limit(0);
 
-// Support running from within another projects vendor directory.
-$backDir = '..'.DIRECTORY_SEPARATOR;
-$pharRoots = [
-    __DIR__.DIRECTORY_SEPARATOR.$backDir,
-    __DIR__.DIRECTORY_SEPARATOR.$backDir.$backDir.$backDir.$backDir,
-];
+$pharRoot = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR;
+$pharAutoload = $pharRoot.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 
-foreach ($pharRoots as $pharRoot) {
-    $pharAutoload = $pharRoot.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
-    if (file_exists($pharAutoload)) {
-        $autoload = include_once $pharAutoload;
-    }
-}
-if (empty($autoload)) {
+if (file_exists($pharAutoload)) {
+    $autoload = include_once $pharAutoload;
+} else {
     echo ' Something is wrong with your drupal.phar archive.'.PHP_EOL.
          ' Try downloading again by executing from your terminal:'.PHP_EOL.
          ' curl https://drupalconsole.com/installer -L -o drupal.phar'.PHP_EOL;
 
     exit(1);
 }
-
 
 $argvInputReader = new ArgvInputReader();
 $drupalConsole = new DrupalConsoleCore($pharRoot);
